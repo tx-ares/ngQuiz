934204f0aa0ebe3561b7fd845973a17127f68b3f
TxsAdamWest <Twinadanz@gmail.com>
2016-11-27 21:29:16 -0600

    starter pokemon created

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -5,7 +5,35 @@
 		.controller("listCtrl", ListController); // Then we can CHAIN a function once it's fetched.  We're going to set it to the controller "listCtrl" and pass it a function.
 
 		function ListController(){ //$scope is an Angular service.  
+			var vm = this; //view model
 
-			this.dummyData = "I want to be the very best!";
+			vm.data = pokeData;
 		};
+
+	var pokeData = [
+		{
+			name: "Squirtle",
+			image_url: "https://2.bp.blogspot.com/-atzUuc_vK_Y/VuYP0hNemgI/AAAAAAAAF8s/3G9YSKC439k3x14QbuWOcvXJVEMxaFZbA/s1600/Squirtle.PNG",
+			type: "Water"
+		},
+
+		{
+			name: "Charmander",
+			image_url: "http://vignette1.wikia.nocookie.net/stephenking/images/5/56/Charmander.png/revision/latest?cb=20131102233219",
+			type: "Fire"
+		},
+
+		{
+			name: "Bulbasaur",
+			image_url: "http://images2.wikia.nocookie.net/__cb20111221113429/bulbasaurfanon/images/4/43/Bulbasaur.png",
+			type: "Grass"
+		},
+
+		{
+			name: "Pikachu",
+			image_url: "http://cartoonbros.com/wp-content/uploads/2016/08/pikachu-6.png",
+			type: "Lightning"
+		}
+
+	]
 })();
