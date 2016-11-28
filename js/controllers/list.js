a6a95a47419780026f0710418ad9a570f31529ce
TxsAdamWest <Twinadanz@gmail.com>
2016-11-27 20:48:51 -0600

    service created

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -4,7 +4,8 @@
 		.module("pokemonFacts") //This time we will fetch our module "pokemonFacts"
 		.controller("listCtrl", ListController); // Then we can CHAIN a function once it's fetched.  We're going to set it to the controller "listCtrl" and pass it a function.
 
-		function ListController(){
+		function ListController(){ //$scope is an Angular service.  
 
+			this.dummyData = "I want to be the very best!";
 		};
 })();
