a35bcc87167a50729fe93e808de02bea3951968b
TxsAdamWest <Twinadanz@gmail.com>
2016-12-29 23:07:22 -0600

    updated angular to 1.6.1

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -4,15 +4,17 @@
 		.module("pokemonFacts") //This time we will fetch our module "pokemonFacts"
 		.controller("listCtrl", ListController); // Then we can CHAIN a function once it's fetched.  We're going to set it to the controller "listCtrl" and pass it a function.
 
-		
+		ListController.$inject = ['quizMetrics'];
 
-		function ListController(){ //$scope is an Angular service.  ( Opting NOT to use $scope in this example. )
+		function ListController(quizMetrics){ //$scope is an Angular service.  ( Opting NOT to use $scope in this example. )
 			var vm = this; //view model
+			console.log(quizMetrics)
+			vm.quizMetrics = quizMetrics;
 
 			vm.data = pokeData;
 			vm.changeActivePokemon = changeActivePokemon; //This takes whatever selected pokemon by the button and "loads" it into the modal.
 			vm.search = "";
-			vm.quizActive = false; //Start our ng-hide property as false to keep our main control div visible until conditions are met to hide it.
+			//vm.quizActive = false; //Start our ng-hide property as false to keep our main control div visible until conditions are met to hide it.
 			vm.activateQuiz = activateQuiz;
 
 			function changeActivePokemon(index) {
@@ -20,7 +22,7 @@
 			};
 
 			function activateQuiz() {
-				vm.quizActive = true;
+				vm.quizMetrics.quizActive = true;
 			};
 		};
 
