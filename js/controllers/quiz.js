f1ec4ce2492f4a606c8b885b3c28649e31a50abb
TxsAdamWest <Twinadanz@gmail.com>
2017-01-24 00:15:17 -0600

    figure out whats wrong with question repeater

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -14,7 +14,10 @@
 
 			vm.quizMetrics = quizMetrics;
 			vm.DataService = dataService;
+			vm.questionAnswered = questionAnswered;
 			vm.activeQuestion = 0; //By setting this value to 0 we can give the questions a starting place, which will later be able to make the questions clickable and will change current view to selected question.
+		
+			function questionAnwered(){}
 		}
 
 
