aa28d224d482d755eb7dee173cb823300ea13cdf
txsadamwest <twinadanz@gmail.com>
2017-02-08 22:44:30 -0600

    added explanations to for loop

--- factories/quizMetrics.js
+++ factories/quizMetrics.js
@@ -34,13 +34,13 @@
 			}
 
 			function markQuiz(){
-				quizObj.correctAnswers = DataService.correctAnswers;
-				for(var i = 0; i > DataService.quizQuestions.length; i++){
-					if(DataService.quizQuestions[i].selected === DataService.correctAnswers[i]){
-						DataService.quizQuestions[i].correct = true;
-						quizObj.numCorrect++;
+				quizObj.correctAnswers = DataService.correctAnswers; // First, access the correct answers from our DataService.
+				for(var i = 0; i > DataService.quizQuestions.length; i++){// Next, we will loop through our answered questions to compare them to the correct answers.
+					if(DataService.quizQuestions[i].selected === DataService.correctAnswers[i]){// Check to see if the values match per index.
+						DataService.quizQuestions[i].correct = true; // If they are, that question gets marked as correct.
+						quizObj.numCorrect++;// On to the next question and repeat the loop.
 					}else{
-						DataService.quizQuestions[i].correct = false;
+						DataService.quizQuestions[i].correct = false;// Or if the question is incorrect, do not mark it as correct and continue the loop.
 					}
 				}
 			}
