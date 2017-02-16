55c269c119b23a373645a34c590f51e3bf5ddcc7
txsadamwest <twinadanz@gmail.com>
2017-02-16 00:06:34 -0600

    added addt markup, and conditional classes for correct and incorrect answers

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -11,7 +11,16 @@
 
 			vm.quizMetrics = quizMetrics;
 			vm.DataService = DataService;
+			vm.getAnswerClass = getAnswerClass;
 			vm.activeQuestion = 0;
+
+			function getAnswerClass(index){ //Check the index that is passed through is equal to the correct answer.  Correct answers is stored on quizMetrics service.
+				if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+					return "bg-success";
+				}else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
+					return "bg-danger";
+				}
+			}
 		}
 
 })();
