e761beecb8e1e6d8e324177ec503d88ec1a97abf
txsadamwest <twinadanz@gmail.com>
2017-02-26 21:47:17 -0600

    refactor results.js

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -1,49 +1,49 @@
-(function(){
+(function() {
 
-	angular
-		.module("PokemonFacts")
-		.controller('resultsCtrl', ResultsController);
+    angular
+        .module("PokemonFacts")
+        .controller('resultsCtrl', ResultsController);
 
-		ResultsController.$inject = ['quizMetrics', 'DataService'];
+    ResultsController.$inject = ['quizMetrics', 'DataService'];
 
-		function ResultsController(quizMetrics, DataService){
-			var vm = this;
+    function ResultsController(quizMetrics, DataService) {
+        var vm = this;
 
-			vm.quizMetrics = quizMetrics;
-			vm.DataService = DataService;
-			vm.getAnswerClass = getAnswerClass;
-			vm.setActiveQuestion = setActiveQuestion;
-			vm.reset = reset;
-			vm.calculatePerc = calculatePerc;
-			vm.activeQuestion = 0;
+        vm.quizMetrics = quizMetrics;
+        vm.dataService = DataService;
+        vm.getAnswerClass = getAnswerClass;
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.reset = reset;
+        vm.calculatePerc = calculatePerc;
+        vm.activeQuestion = 0;
 
-			function calculatePerc(){
-				return quizMetrics.numCorrect / DataService.quizQuestions.length * 100
-			}
+        function calculatePerc() {
+            return quizMetrics.numCorrect / DataService.quizQuestions.length * 100;
+        }
 
-			function setActiveQuestion(index){//A simple purpose of this function is to set active question to the index during the ng-repeat loop.
-				vm.activeQuestion = index;
-			}
+        function setActiveQuestion(index) { //A simple purpose of this function is to set active question to the index during the ng-repeat loop.
+            vm.activeQuestion = index;
+        }
 
-			function getAnswerClass(index){ //Check the index that is passed through is equal to the correct answer.  Correct answers is stored on quizMetrics service.
-				if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
-					return "bg-success";
-				}else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
-					return "bg-danger";
-				}
-			}
+        function getAnswerClass(index) { //Check the index that is passed through is equal to the correct answer.  Correct answers is stored on quizMetrics service.
+            if (index === quizMetrics.correctAnswers[vm.activeQuestion]) {
+                return "bg-success";
+            } else if (index === DataService.quizQuestions[vm.activeQuestion].selected) {
+                return "bg-danger";
+            }
+        }
 
-			function reset() {//This will reset all flags for each question to their default null values, getting it ready for the next pokemaster!
-				quizMetrics.changeState("results", false);
-				quizMetrics.numCorrect = 0;
+        function reset() { //This will reset all flags for each question to their default null values, getting it ready for the next pokemaster!
+            quizMetrics.changeState("results", false);
+            quizMetrics.numCorrect = 0;
 
-				for(var i = 0; i < DataService.quizQuestions.length; i++){
-					var data = DataService.quizQuestions[i];
+            for (var i = 0; i < DataService.quizQuestions.length; i++) {
+                var data = DataService.quizQuestions[i];
 
-					data.selected = null;
-					data.correct = null;
-				}
-			}
-		}
+                data.selected = null;
+                data.correct = null;
+            }
+        }
+    }
 
 })();
